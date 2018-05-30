8618159a18e2086e82a78d0b7ec6525362c3833f
Dmitriy Kononov <kondor_06@mail.ru>
2018-05-30 09:45:46 +0300

    30.05.2018

--- js/main.js
+++ js/main.js
@@ -1,16 +1,32 @@
 "use strict";
 
-const SCREENS = [`central`, `greeting`, `rules`, `game-2`, `game-1`, `game-3`,
+const SCREENS = [`central`, `greeting`, `rules`, `game-1`, `game-2`, `game-3`,
   `stats`];
 const Keys = {
   'LEFT': 37,
   'RIGHT': 39
 };
 
-const mainNode = document.querySelector(`.central`);
+const BUTTON_ARROW_STYLE =
+  `<style>
+    .arrows__wrap {
+      position: absolute;
+      top: 95px;
+      left: 50%;
+      margin-left: -56px;
+    }
+    .arrows__btn {
+      background: none;
+      border: 2px solid black;
+      padding: 5px 20px;
+    }
+  </style>`;
+
+const bodyNode = document.querySelector(`body`);
+const mainNode = bodyNode.querySelector(`.central`);
 
 let screensNodes = [];
-let screenNum = 0;
+let curScr = 0;
 
 const getScreensNodes = () => {
 
@@ -20,38 +36,64 @@ const getScreensNodes = () => {
       document.querySelector(`#${screen}`);
 
     screensNodes.push(node);
-
   });
 };
 
-const drawScreen = (ScrNum) => {
-  mainNode.innerHTML = screensNodes[ScrNum].innerHTML;
-};
-
-const switchScreen = (switchRight) => {
-  if (switchRight) {
-    screenNum += (screenNum < screensNodes.length - 1) ? 1 : 0;
-    return true;
+const switchScreen = (scrNum) => {
+  if (scrNum < 0) {
+    scrNum = 0;
+  }
+  if (scrNum > screensNodes.length - 1) {
+    scrNum = screensNodes.length - 1;
   }
 
-  screenNum -= (screenNum > 0) ? 1 : 0;
-  return true;
+  mainNode.innerHTML = screensNodes[scrNum].innerHTML;
+  return scrNum;
+};
+
+const createGraphicalArrow = () => {
+  const createButton = (val) => {
+    let buttonNode = document.createElement(`button`);
+
+    buttonNode.classList.add(`arrows__btn`);
+    buttonNode.appendChild(document.createTextNode(val));
+
+    return buttonNode;
+  };
+
+  const arrowNode = document.createElement(`div`);
+
+  arrowNode.classList.add(`arrows__wrap`);
+  arrowNode.innerHTML = BUTTON_ARROW_STYLE;
+
+  let buttonLeft = createButton(`<-`);
+  let buttonRight = createButton(`->`);
+
+  buttonLeft.addEventListener(`click`, () => {
+    curScr = switchScreen(--curScr);
+  });
+  buttonRight.addEventListener(`click`, () => {
+    curScr = switchScreen(++curScr);
+  });
+
+  arrowNode.appendChild(buttonLeft);
+  arrowNode.appendChild(buttonRight);
+
+  bodyNode.appendChild(arrowNode);
 };
 
 getScreensNodes();
+createGraphicalArrow();
+
 document.addEventListener(`keydown`, (evt) => {
   switch (evt.keyCode) {
     case Keys.LEFT:
       evt.preventDefault();
-      switchScreen(false);
-      drawScreen(screenNum);
-
+      curScr = switchScreen(--curScr);
       break;
     case Keys.RIGHT:
       evt.preventDefault();
-      switchScreen(true);
-      drawScreen(screenNum);
+      curScr = switchScreen(++curScr);
       break;
-
   }
 });
